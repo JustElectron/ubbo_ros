8d23fabec3ebfc1cf4528f5eea37cafbf8cbc857
Andreas Rønning <and_just1@hotmail.com>
2019-03-29 15:34:31 +0100

    first commit

---
+++ include/ubbo_ROS/ubbo_ROS.h
@@ -0,0 +1,12 @@
+#ifndef _UBBO_ROS_H_
+#define _UBBO_ROS_H_
+
+#include <ros/ros.h>
+
+#include "ubbo/ubbo.h"
+
+
+
+
+
+#endif // _UBBO_ROS_H_
---
+++ src/ubbo_ROS.cpp
@@ -0,0 +1,8 @@
+
+#include "ubbo_ROS/ubbo_ROS.h"
+
+
+int main(int argc, char** argv)
+{
+
+}
